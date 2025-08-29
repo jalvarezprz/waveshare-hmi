04c8b3888afd1f682425806232afe53d728bc088
jalvarezprz <joaquin.alvarez.prz@gmail.com>
2025-08-29 10:23:08 +0200

    Modificación irrelevante en main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -7,17 +7,18 @@
 
 extern "C" void app_main(void)
 {
-    /*
+    
     ESP_ERROR_CHECK(waveshare_esp32_s3_rgb_lcd_init());
 
     lvgl_port_lock(UINT32_MAX);   // bloquea (equivalente a -1)
     ui_init_example();
     lvgl_port_unlock();
-    */
     
+    /*
     ESP_LOGI("MAIN", "Arrancando HMI con menús MCR50...");
     // Depuración: imprime los menús embebidos
     printMenuMcr50();
+    */
 
     while (true) vTaskDelay(pdMS_TO_TICKS(1000));
 }
